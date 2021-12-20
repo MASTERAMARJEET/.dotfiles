bb7f5320287a95c596f4166f7d3ec9abe9281e0b
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2021-12-20 15:24:43 +0530

    set keymaps to liking

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -1,69 +1,70 @@
 local opts = { noremap = true, silent = true }
+local nosilent_opts = { silent = true }
 
 local term_opts = { silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
---Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+--Remap "," as leader key
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 -- Modes
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
+local mode = {
+  normal = "n",
+  insert = "i",
+  visual = "v",
+  visual_only = "x",
+  term = "t",
+  command = "c",
+}
 
--- Normal --
+--
+keymap(mode.normal, ";", ':', nosilent_opts)
+
+-- disable arrow keys in normal mode
+keymap(mode.normal, "<Down>", "<Nop>", opts)
+keymap(mode.normal, "<Up>", "<Nop>", opts)
+keymap(mode.normal, "<Left>", "<Nop>", opts)
+keymap(mode.normal, "<Right>", "<Nop>", opts)
+
+-- Better copy paste
+keymap(mode.normal, "<leader>y", '"+y', opts)
+keymap(mode.normal, "<leader>p", '"+p', opts)
+keymap(mode.visual, "p", '"_dP', opts)
+keymap(mode.normal, "<A-Up>", 'YP', opts)
+keymap(mode.normal, "<A-Down>", 'Yp', opts)
+
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap(mode.normal, "<C-h>", "<C-w>h", opts)
+keymap(mode.normal, "<C-j>", "<C-w>j", opts)
+keymap(mode.normal, "<C-k>", "<C-w>k", opts)
+keymap(mode.normal, "<C-l>", "<C-w>l", opts)
+keymap(mode.insert, "<C-h>", "<C-\\><C-N><C-w>h", opts)
+keymap(mode.insert, "<C-j>", "<C-\\><C-N><C-w>j", opts)
+keymap(mode.insert, "<C-k>", "<C-\\><C-N><C-w>k", opts)
+keymap(mode.insert, "<C-l>", "<C-\\><C-N><C-w>l", opts)
+keymap(mode.term, "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap(mode.term, "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap(mode.term, "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap(mode.term, "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Move text up and down
+keymap(mode.visual, "J", ":move '>+1<CR>gv=gv", opts)
+keymap(mode.visual, "K", ":move '<-2<CR>gv=gv", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap(mode.normal, "<C-Up>", ":resize -2<CR>", opts)
+keymap(mode.normal, "<C-Down>", ":resize +2<CR>", opts)
+keymap(mode.normal, "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap(mode.normal, "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap(mode.normal, "L", ":bnext<CR>", opts)
+keymap(mode.normal, "H", ":bprevious<CR>", opts)
 
--- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
-
--- Insert --
--- Press jk fast to enter
-keymap("i", "jk", "<ESC>", opts)
-
--- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
-
--- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
-
--- Visual Block --
--- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
-
--- Terminal --
--- Better terminal navigation
--- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
--- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
--- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
--- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+keymap(mode.visual_only, "<", "<gv", opts)
+keymap(mode.visual_only, ">", ">gv", opts)
 
