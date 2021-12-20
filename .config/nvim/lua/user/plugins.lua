047c93b85ca52cbe187789554ac8bb943df619d5
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2021-12-21 00:35:15 +0530

    change nvim plugins

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -41,58 +41,59 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
   -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-  use "numToStr/Comment.nvim" -- Easily comment stuff
-  use "kyazdani42/nvim-web-devicons"
-  use "kyazdani42/nvim-tree.lua"
-  use "akinsho/bufferline.nvim"
-  use "moll/vim-bbye"
-  use "nvim-lualine/lualine.nvim"
-  use "akinsho/toggleterm.nvim"
-  use "ahmedkhalf/project.nvim"
-  use "lewis6991/impatient.nvim"
-  use "lukas-reineke/indent-blankline.nvim"
-  use "goolord/alpha-nvim"
-  use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
-  use "folke/which-key.nvim"
+  use { "wbthomason/packer.nvim"  } -- Have packer manage itself
+  use { "nvim-lua/popup.nvim"  } -- An implementation of the Popup API from vim in Neovim
+  use { "nvim-lua/plenary.nvim"  } -- Useful lua functions used ny lots of plugins
+  use { "windwp/nvim-autopairs"  } -- Autopairs, integrates with both cmp and treesitter
+  use { "numToStr/Comment.nvim"  } -- Easily comment stuff
+  use { "kyazdani42/nvim-web-devicons" }
+  use { "kyazdani42/nvim-tree.lua" }
+  use { "akinsho/bufferline.nvim" }
+  use { "moll/vim-bbye" }
+  use { "nvim-lualine/lualine.nvim" }
+  use { "akinsho/toggleterm.nvim" }
+  use { "ahmedkhalf/project.nvim" }
+  use { "lewis6991/impatient.nvim" }
+  use { "lukas-reineke/indent-blankline.nvim" }
+  use { "goolord/alpha-nvim" }
+  use { "antoinemadec/FixCursorHold.nvim"  }-- This is needed to fix lsp doc highlight
+  use { "folke/which-key.nvim" }
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
+  -- use { "lunarvim/darkplus.nvim" }
+  use { "kaicataldo/material.vim", branch = 'main' }
 
   -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
+  use { "hrsh7th/nvim-cmp"  }-- The completion plugin
+  use { "hrsh7th/cmp-buffer"  }-- buffer completions
+  use { "hrsh7th/cmp-path"  }-- path completions
+  use { "hrsh7th/cmp-cmdline"  }-- cmdline completions
+  use { "saadparwaiz1/cmp_luasnip"  }-- snippet completions
+  use { "hrsh7th/cmp-nvim-lsp" }
 
   -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  use { "L3MON4D3/LuaSnip"  }--snippet engine
+  use { "rafamadriz/friendly-snippets"  }-- a bunch of snippets to use
 
   -- LSP
-  use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use { "neovim/nvim-lspconfig"  }-- enable LSP
+  use { "williamboman/nvim-lsp-installer"  }-- simple to use language server installer
+  use { "tamago324/nlsp-settings.nvim"  }-- language server settings defined in json for
+  use { "jose-elias-alvarez/null-ls.nvim"  }-- for formatters and linters
 
   -- Telescope
-  use "nvim-telescope/telescope.nvim"
+  use { "nvim-telescope/telescope.nvim" }
 
   -- Treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
-  use "JoosepAlviste/nvim-ts-context-commentstring"
+  use { "JoosepAlviste/nvim-ts-context-commentstring" }
 
   -- Git
-  use "lewis6991/gitsigns.nvim"
+  use { "lewis6991/gitsigns.nvim" }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
