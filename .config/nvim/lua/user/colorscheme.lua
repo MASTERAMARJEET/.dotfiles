8384c7b9d68feaa6e5a62cf4caae98a711657599
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2021-12-21 00:35:38 +0530

    use material ocean colortheme

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,10 @@
+vim.cmd[[highlight ColorColumn ctermbg=0 guibg=lightgrey]]
+vim.opt.termguicolors = true
+vim.g.material_terminal_italics = 1
+vim.g.material_theme_style = "ocean-community"
 vim.cmd [[
 try
-  colorscheme darkplus
+  colorscheme material
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
