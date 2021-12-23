6340637b7646bae737b9a67acb060c6c9db13137
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2021-12-24 00:02:18 +0530

    add FixCursorHold

--- .config/nvim/lua/installs.lua
+++ .config/nvim/lua/installs.lua
@@ -77,6 +77,7 @@ return packer.startup(function(use)
   use({ "ahmedkhalf/project.nvim" })
 
   -- MISCS
+  use({ "antoinemadec/FixCursorHold.nvim" }) -- This is needed to fix lsp doc highlight
   use({ "folke/which-key.nvim" })
 
   -- BETTER UI
