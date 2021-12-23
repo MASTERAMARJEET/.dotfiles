0b6a4fb6f913e481f6ad163087c1307b5cf5fcc1
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2021-12-23 12:04:21 +0530

    configure nvim-tree to liking

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,25 +1,67 @@
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
+local vim_globals = {
+  quit_on_open = 0,
+  indent_markers = 0,
+  git_hl = 1,
+  highlight_opened_files = 1,
+  root_folder_modifier = ":t",
+  add_trailing = 0,
+  group_empty = 1,
+  disable_window_picker = 1,
+  icon_padding = ' ',
+  symlink_arrow = ' >> ',
+  respect_buf_cwd = 1,
+  create_in_closed_folder = 0,
+  refresh_wait = 500,
+  window_picker_exclude = {
+    filetype = {
+      'notify',
+      'packer',
+      'qf',
+    },
+    buftype = {
+      'terminal'
+    }
   },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
+  special_files = {
+    ['README.md'] = 1,
+    ['Makefile'] = 1,
+    ['MAKEFILE'] = 1,
   },
+  show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1,
+    folder_arrows = 0,
+  },
+  icons = {
+    default = "",
+    symlink = "",
+    git = {
+      unstaged = "",
+      staged = "S",
+      unmerged = "",
+      renamed = "➜",
+      deleted = "",
+      untracked = "U",
+      ignored = "◌",
+    },
+    folder = {
+      default = "",
+      open = "",
+      empty = "",
+      empty_open = "",
+      symlink = "",
+    },
+  }
 }
+
+local function global_setup(global_config)
+  for key, value in pairs(global_config) do
+    vim.g['nvim_tree_' .. key] = value
+  end
+end
+
+global_setup(vim_globals)
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
@@ -31,6 +73,7 @@ if not config_status_ok then
   return
 end
 
+local keymaps = require('keymaps.nvim-tree')
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
@@ -44,7 +87,7 @@ nvim_tree.setup {
   },
   auto_close = true,
   open_on_tab = false,
-  hijack_cursor = false,
+  hijack_cursor = true,
   update_cwd = true,
   update_to_buf_dir = {
     enable = true,
@@ -70,7 +113,9 @@ nvim_tree.setup {
   },
   filters = {
     dotfiles = false,
-    custom = {},
+    custom = {
+      '.git'
+    },
   },
   git = {
     enable = true,
@@ -81,15 +126,11 @@ nvim_tree.setup {
     width = 30,
     height = 30,
     hide_root_folder = false,
-    side = "left",
+    side = "right",
     auto_resize = true,
     mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
+      custom_only = true,
+      list = keymaps
     },
     number = false,
     relativenumber = false,
@@ -97,16 +138,5 @@ nvim_tree.setup {
   trash = {
     cmd = "trash",
     require_confirm = true,
-  },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
   },
 }
