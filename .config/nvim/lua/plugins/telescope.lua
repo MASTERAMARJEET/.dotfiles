b6e12a9f7cf6914ce9c3b636db0634c365323b5e
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2021-12-23 11:30:07 +0530

    improve telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -85,6 +85,7 @@ telescope.setup {
     -- }
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
+    find_files = { hidden = true },
   },
   extensions = {
     -- Your extension configuration goes here:
@@ -92,5 +93,14 @@ telescope.setup {
     --   extension_config_key = value,
     -- }
     -- please take a look at the readme of the extension you want to configure
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
   },
 }
+telescope.load_extension('fzf')
+
