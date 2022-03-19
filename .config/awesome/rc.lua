d3d499c93899229b1dc8b0fc099ba856fbb9fcbd
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-03-19 15:43:53 +0530

    remove useless from awesome topbar

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -38,13 +38,11 @@ end)
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
--- This is used later as the default terminal and editor to run.
 terminal = "st"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
 modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
@@ -69,37 +67,6 @@ awful.layout.layouts = {
 -- }}}
 
 -- {{{ Menu
--- Create a launcher widget and a main menu
-myawesomemenu = {
-    {
-        "hotkeys",
-        function()
-            hotkeys_popup.show_help(nil, awful.screen.focused())
-        end,
-    },
-    { "manual", terminal .. " -e man awesome" },
-    { "edit config", editor_cmd .. " " .. awesome.conffile },
-    { "restart", awesome.restart },
-    {
-        "quit",
-        function()
-            awesome.quit()
-        end,
-    },
-}
-
-mymainmenu = awful.menu({
-    items = {
-        { "awesome", myawesomemenu, beautiful.awesome_icon },
-        { "open terminal", terminal },
-    },
-})
-
-mylauncher = awful.widget.launcher({
-    image = beautiful.awesome_icon,
-    menu = mymainmenu,
-})
-
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
@@ -126,9 +93,6 @@ end)
 
 -- {{{ Wibar
 
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
 
@@ -139,9 +103,6 @@ screen.connect_signal("request::desktop_decoration", function(s)
         s,
         awful.layout.layouts[1]
     )
-
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
 
     -- Create an imagebox widget which will contain an icon indicating which layout we're using.
     -- We need one layoutbox per screen.
@@ -222,14 +183,11 @@ screen.connect_signal("request::desktop_decoration", function(s)
             layout = wibox.layout.align.horizontal,
             { -- Left widgets
                 layout = wibox.layout.fixed.horizontal,
-                mylauncher,
                 s.mytaglist,
-                s.mypromptbox,
             },
             s.mytasklist, -- Middle widget
             { -- Right widgets
                 layout = wibox.layout.fixed.horizontal,
-                mykeyboardlayout,
                 wibox.widget.systray(),
                 mytextclock,
                 s.mylayoutbox,
@@ -241,9 +199,6 @@ end)
 
 -- {{{ Mouse bindings
 awful.mouse.append_global_mousebindings({
-    awful.button({}, 3, function()
-        mymainmenu:toggle()
-    end),
     awful.button({}, 4, awful.tag.viewprev),
     awful.button({}, 5, awful.tag.viewnext),
 })
@@ -493,8 +448,6 @@ awful.keyboard.append_global_keybindings({
         awful.util.spawn("brave")
     end, { description = "open browser", group = "launcher" }),
 })
-
-
 
 client.connect_signal("request::default_keybindings", function()
     awful.keyboard.append_client_keybindings({
