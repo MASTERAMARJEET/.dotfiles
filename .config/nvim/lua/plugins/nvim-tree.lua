3b11dbe8554c51a8fe1c4c653f7d59b72191fba8
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-03-19 15:16:42 +0530

    update nvim-tree config

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,27 +1,15 @@
 local vim_globals = {
-  quit_on_open = 0,
   indent_markers = 0,
   git_hl = 1,
   highlight_opened_files = 1,
   root_folder_modifier = ":t",
   add_trailing = 0,
   group_empty = 1,
-  disable_window_picker = 1,
   icon_padding = " ",
   symlink_arrow = " >> ",
   respect_buf_cwd = 1,
   create_in_closed_folder = 0,
   refresh_wait = 500,
-  window_picker_exclude = {
-    filetype = {
-      "notify",
-      "packer",
-      "qf",
-    },
-    buftype = {
-      "terminal",
-    },
-  },
   special_files = {
     ["README.md"] = 1,
     ["Makefile"] = 1,
@@ -135,6 +123,31 @@ nvim_tree.setup({
     },
     number = false,
     relativenumber = false,
+  },
+  actions = {
+    change_dir = {
+      enable = true,
+      global = false,
+    },
+    open_file = {
+      quit_on_open = false,
+      resize_window = false,
+      window_picker = {
+        enable = true,
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = {
+            "notify",
+            "packer",
+            "qf",
+            "diff",
+            "fugitive",
+            "fugitiveblame",
+          },
+          buftype = { "nofile", "terminal", "help" },
+        },
+      },
+    },
   },
   trash = {
     cmd = "trash",
