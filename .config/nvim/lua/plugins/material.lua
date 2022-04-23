1240c9a14550e80fb3e3d18cc956d10c7d473f81
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-04-23 12:10:23 +0530

    (nvim) update material colorscheme

--- .config/nvim/lua/plugins/material.lua
+++ .config/nvim/lua/plugins/material.lua
@@ -1,7 +1,15 @@
 require("material").setup({
+  contrast = {
+    sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+    floating_windows = true, -- Enable contrast for floating windows
+    line_numbers = false, -- Enable contrast background for line numbers
+    sign_column = false, -- Enable contrast background for the sign column
+    cursor_line = true, -- Enable darker background for the cursor line
+    non_current_windows = false, -- Enable darker background for non-current windows
+    popup_menu = false, -- Enable lighter background for the popup menu
+  },
 
-  contrast = true, -- Enable contrast for sidebars, floating windows and popup menus like Nvim-Tree
-  borders = false, -- Enable borders between verticaly split windows
+  borders = true, -- Enable borders between verticaly split windows
 
   popup_menu = "dark", -- Popup menu style ( can be: 'dark', 'light', 'colorful' or 'stealth' )
 
@@ -13,6 +21,28 @@ require("material").setup({
     variables = false, -- Enable italic variables
   },
 
+  contrast_filetypes = { -- Specify which filetypes get the contrasted (darker) background
+    "terminal", -- Darker terminal background
+    "packer", -- Darker packer background
+    "qf", -- Darker qf list background
+  },
+
+  high_visibility = {
+    lighter = false, -- Enable higher contrast text for lighter style
+    darker = true, -- Enable higher contrast text for darker style
+  },
+
+  disable = {
+    borders = false, -- Disable borders between verticaly split windows
+    background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
+    term_colors = false, -- Prevent the theme from setting terminal colors
+    eob_lines = false, -- Hide the end-of-buffer lines
+  },
+
+  lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+
+  async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+
   contrast_windows = { -- Specify which windows get the contrasted (darker) background
     "terminal", -- Darker terminal background
     "packer", -- Darker packer background
@@ -22,12 +52,6 @@ require("material").setup({
   text_contrast = {
     lighter = false, -- Enable higher contrast text for lighter style
     darker = true, -- Enable higher contrast text for darker style
-  },
-
-  disable = {
-    background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
-    term_colors = false, -- Prevent the theme from setting terminal colors
-    eob_lines = false, -- Hide the end-of-buffer lines
   },
 
   custom_highlights = {}, -- Overwrite highlights with your own
